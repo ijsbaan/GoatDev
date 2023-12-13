e4b1edf00f77df06780a1b7134593a1bc92d3ce2
susant Budhathoki <susant.budhathoki@hva.nl>
2023-12-13 14:23:20 +0100

    pickup logic with  temp movement

--- Assets/Scripts/Interface/Ipickup.cs
+++ Assets/Scripts/Interface/Ipickup.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ipickup : MonoBehaviour
+public interface Ipickup 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public void Pickup();
 }
---
+++ Assets/Scripts/Temp_movement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Temp_movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 7;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.W))
+        {
+            input.y += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            input.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            input.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            input.x += 1;
+        }
+
+        Vector3 dir = new Vector3(input.x, input.y, 0);
+        transform.position += dir * moveSpeed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Ipickup ipickup = collision.gameObject.GetComponent<Ipickup>();
+        if(ipickup != null )
+        {
+            ipickup.Pickup();
+        }
+    }
+}
