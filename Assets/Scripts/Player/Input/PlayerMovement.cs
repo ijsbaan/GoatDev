d0d3adc677f86283160dbf3bdbd623eaa0dc3b18
Vincent Chang <vincent.chang@hva.nl>
2023-12-14 11:03:17 +0100

    Added movement

---
+++ Assets/Scripts/Player/Input/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+using UnityEngine.Windows;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private InputPlayer inputPlayer;
+    public int speed = 10;
+    void Awake()
+    {
+        inputPlayer = new InputPlayer();
+        inputPlayer.Player.Movement.Enable();
+    }
+
+    public Vector2 GetInputValue()
+    {
+        Vector2 input = inputPlayer.Player.Movement.ReadValue<Vector2>();
+        
+        Vector3 dir = new Vector3(input.x, input.y, 0).normalized;
+        transform.position += dir * speed * Time.deltaTime;
+            
+        return input;
+    }
+
+    private void Update()
+    {
+        GetInputValue();
+       // Debug.Log(GetInputValue());
+    }
+}
