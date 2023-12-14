3da2896ab483a3cc2d9bf448b7b141dc18a08eae
Vincent Chang <vincent.chang@hva.nl>
2023-12-14 15:14:39 +0100

    Added camera for chest adjusted code and scripts

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Chest : MonoBehaviour, Iinteractable
-{ 
-
+{
+    [SerializeField] private GameObject camera;
+    private bool isInteracting;
     public void Interact()
-    {
+    { 
+
         Debug.Log("interact");
-        GameObject.Destroy(gameObject);
+
+        isInteracting = !isInteracting;
+        //GameObject.Destroy(gameObject);
+        if (isInteracting)  camera.SetActive(true);
+        else camera.SetActive(false);
     }
+
 }
