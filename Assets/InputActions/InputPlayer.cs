3fff1a7490f61a2ec21115ec1ffcdc6b3f68a2af
Vincent Chang <vincent.chang@hva.nl>
2023-12-18 18:43:00 +0100

    Added walking and running with joystick

--- Assets/InputActions/InputPlayer.cs
+++ Assets/InputActions/InputPlayer.cs
@@ -48,61 +48,6 @@ public partial class @InputPlayer : IInputActionCollection2, IDisposable
             ],
             ""bindings"": [
                 {
-                    ""name"": ""2D Vector"",
-                    ""id"": ""866c6148-26e7-4ea6-a43f-598653dd2a7f"",
-                    ""path"": ""2DVector"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Movement"",
-                    ""isComposite"": true,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": ""up"",
-                    ""id"": ""349ca8af-dca2-47b0-aaa0-26b353ee2d0e"",
-                    ""path"": ""<Gamepad>/leftStick/up"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Keyboard"",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""down"",
-                    ""id"": ""4da17dda-ad88-4202-bba9-857c32b9b55c"",
-                    ""path"": ""<Gamepad>/leftStick/down"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Keyboard"",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""left"",
-                    ""id"": ""7ad7c4d7-bf4f-4a89-a614-9eaac3ebcdc2"",
-                    ""path"": ""<Gamepad>/leftStick/left"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Keyboard"",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
-                    ""name"": ""right"",
-                    ""id"": ""147d4fd7-d366-408c-ad62-063ee7428486"",
-                    ""path"": ""<Gamepad>/leftStick/right"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": ""Keyboard"",
-                    ""action"": ""Movement"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": true
-                },
-                {
                     ""name"": ""wasd"",
                     ""id"": ""5eceed95-47d3-4abe-8f6a-0bd4e0acc975"",
                     ""path"": ""2DVector"",
@@ -156,6 +101,17 @@ public partial class @InputPlayer : IInputActionCollection2, IDisposable
                     ""action"": ""Movement"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""ce5255ed-a11d-4b4e-a01f-afcac3615d49"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 },
                 {
                     ""name"": """",
--- Assets/Scripts/Player/Input/PlayerMovement.cs
+++ Assets/Scripts/Player/Input/PlayerMovement.cs
@@ -8,7 +8,9 @@ using UnityEngine.Windows;
 public class PlayerMovement : MonoBehaviour
 {
     private InputPlayer inputPlayer;
-    [SerializeField] private int speed = 10;
+
+    [SerializeField] private float speed = 10;
+    [SerializeField] private float runThreshold = 0.5f;
     void Awake()
     {
         inputPlayer = new InputPlayer();
@@ -18,15 +20,20 @@ public class PlayerMovement : MonoBehaviour
     public Vector2 GetInputValue()
     {
         Vector2 input = inputPlayer.Player.Movement.ReadValue<Vector2>();
-        
+
+        bool isrunning = (input.magnitude > runThreshold) ? true : false;
+
         Vector3 dir = new Vector3(input.x, input.y, 0).normalized;
-        transform.position += dir * speed * Time.deltaTime;
+
+        if (isrunning) transform.position += dir * speed * Time.deltaTime;
+         
+        else transform.position += dir * speed * 0.5f * Time.deltaTime;
             
         return input;
     }
 
 
-    private void FixedUpdate()
+    private void Update()
     {
         GetInputValue();
     }
