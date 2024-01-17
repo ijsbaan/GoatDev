58d514b9cfd34f8067717d049fcb20b45280375d
Vincent Chang <vincent.chang@hva.nl>
2024-01-17 21:27:48 +0100

    Added a recursive function it still doesn't work though

--- Assets/Scripts/Player/Input/PlayerAttack.cs
+++ Assets/Scripts/Player/Input/PlayerAttack.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,35 +6,75 @@ public class PlayerAttack : MonoBehaviour
 {
     private InputPlayer inputActions;
     [SerializeField] private GameObject attackBox;
-    
-
+    private Coroutine attackCoroutine;
+    private bool isAttacking = false;
 
     private void Awake()
     {
         inputActions = new InputPlayer();
         inputActions.Player.Attack.Enable();
-        inputActions.Player.Attack.performed +=Attack_performed;
+        inputActions.Player.Attack.performed += AttackPerformed;
     }
-    private Vector3 movementDirection()
+
+    private Vector3 MovementDirection()
     {
         Vector2 input = inputActions.Player.Movement.ReadValue<Vector2>();
-
         Vector3 dir = new Vector3(input.x, input.y, 0).normalized;
         return dir;
     }
 
-    private void Attack_performed(InputAction.CallbackContext obj)
+    private void AttackPerformed(InputAction.CallbackContext obj)
     {
+        // If an attack is already in progress, do nothing
+        if (isAttacking)
+        {
+            return;
+        }
 
-        StartCoroutine("Attack");
+        // Start a new attack
+        attackCoroutine = StartCoroutine(Attack());
     }
 
     public IEnumerator Attack()
     {
-        var hitbox = Instantiate(attackBox, gameObject.transform.position + movementDirection(), Quaternion.identity);
-        yield return new WaitForSeconds(2);
+        isAttacking = true;
+
+        var hitbox = Instantiate(attackBox, gameObject.transform.position + MovementDirection(), Quaternion.identity);
+
+        // Initiate recursive countdown for hitbox lifetime
+        yield return StartCoroutine(RecursiveCountdownCoroutine(2, hitbox));
+
         Destroy(hitbox);
+        isAttacking = false;
     }
 
+    private IEnumerator RecursiveCountdownCoroutine(float timeRemaining, GameObject hitbox)
+    {
+        // Base case
+        if (timeRemaining <= 0)
+        {
+            yield break;
+        }
 
+        // Check if cancellation condition is met
+        if (CancelCondition())
+        {
+            Debug.Log("Attack cancelled.");
+            yield break;
+        }
+
+        Debug.Log($"Time remaining: {timeRemaining}");
+
+        // Wait for a short duration
+        yield return new WaitForSeconds(0.1f);
+
+        // Recursive call
+        yield return StartCoroutine(RecursiveCountdownCoroutine(timeRemaining - 0.1f, hitbox));
+    }
+
+    private bool CancelCondition()
+    {
+        // Check if the attack input is performed again
+        return inputActions.Player.Attack.triggered;
+    }
 }
