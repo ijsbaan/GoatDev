ae5d7797a39c3136a46f24651915d6ab7addf1b4
Vincent Chang <vincent.chang@hva.nl>
2024-03-01 16:17:24 +0100

    Added a timer to the spawn times

--- Assets/Scripts/Enemy/Flocking/Flock.cs
+++ Assets/Scripts/Enemy/Flocking/Flock.cs
@@ -1,6 +1,9 @@
 using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net;
+using Unity.Collections.LowLevel.Unsafe;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Flock : MonoBehaviour
@@ -9,10 +12,10 @@ public class Flock : MonoBehaviour
     List<FlockAgent> agents = new List<FlockAgent>();
     public FlockBehaviour behaviour;
 
-    [Range(10, 500)]
-    public int startingCount = 250;
+    public int spawnChildren = 1;
     const float AgentDensity = 0.08f;
 
+    public int counter = 0;
     [Range(1f, 100f)]
     public float driveFactor = 10f;
     [Range(1f, 100f)]
@@ -33,19 +36,32 @@ public class Flock : MonoBehaviour
         squareNeighbourRadius = neighbourRadius * neighbourRadius;
         squareAvoidanceRadius = squareNeighbourRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
 
-        for (int i = 0; i < startingCount; i++)
-        {
-            FlockAgent newagent = Instantiate(agentPrefab, Random.insideUnitCircle * startingCount * AgentDensity, Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)), transform);
-            newagent.name = "Agent" + i;
-            agents.Add(newagent);
-        }
+        //for (int i = 0; i < startingCount; i++)
+        //{
+        //    FlockAgent newagent = Instantiate(agentPrefab, Random.insideUnitCircle * startingCount * AgentDensity, Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)), transform);
+        //    newagent.name = "Agent" + i;
+        //    agents.Add(newagent);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(agents.Count < 5) counter++;
+        
+        if(counter > 1000 && agents.Count < 5)
+        { 
+            makeChildren();
+            counter = 0;
+        }
+
         foreach (FlockAgent agent in agents)
         {
+            //if (agent != null)
+            //{
+            //    continue;
+            //}
             List<Transform> context = GetNearbyObjects(agent);
 
             //agent.GetComponentInChildren<SpriteRenderer>().color = Color.Lerp(Color.white, Color.red, context.Count / 6f);
@@ -62,6 +78,15 @@ public class Flock : MonoBehaviour
         }
     }
 
+    private void makeChildren()
+    {
+        for (int i = 0; i < spawnChildren; i++)
+        {
+            FlockAgent newAgent = Instantiate(agentPrefab, Random.insideUnitCircle * AgentDensity, Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)), transform);
+            newAgent.name = "Agent" + i;
+            agents.Add(newAgent);
+        }
+    }
     List<Transform> GetNearbyObjects(FlockAgent agent)
     {
         List<Transform> context = new List<Transform>();
