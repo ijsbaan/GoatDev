4cf04c55819c536713dd4c4626cd5a9920c48986
Vincent Chang <vincent.chang@hva.nl>
2024-03-13 12:32:21 +0100

    Hunting works

--- Assets/Scripts/Enemy/Flocking/BevaviourScripts/SteeredHuntBehaviour.cs
+++ Assets/Scripts/Enemy/Flocking/BevaviourScripts/SteeredHuntBehaviour.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Flock/Behaviour/Spyke")]
-public class HuntBehaviour : FlockBehaviour
+[CreateAssetMenu(menuName = "Flock/Behaviour/SteeredSpyke")]
+public class SteeredHuntBehaviour : FlockBehaviour
 {
     private GameObject target;
 
+    Vector2 currentVelocity;
+    public float agentSmoothTime;
     public override Vector2 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
     {
         target = GameObject.FindGameObjectWithTag("Player");
@@ -19,11 +21,11 @@ public class HuntBehaviour : FlockBehaviour
             return agent.transform.up;
 
         Vector2 huntMove = Vector2.zero;
-            
-        huntMove += (Vector2) (target.transform.position - agent.transform.position);
 
-        huntMove = huntMove.normalized;
+        huntMove += (Vector2)(target.transform.position - agent.transform.position);
+
+        huntMove = Vector2.SmoothDamp(agent.transform.up, huntMove, ref currentVelocity, agentSmoothTime);
 
         return huntMove;
-    }
+    }   
 }
